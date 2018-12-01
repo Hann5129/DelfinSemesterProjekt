8b1e4a8ff0e0993f430bb58994b1f21c5eeb128e
sylv1634 <43637056+sylv1634@users.noreply.github.com>
2018-12-01 12:32:00 +0100

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -5,70 +5,65 @@ import java.io.*;
 public class Manager extends Cashier{
 
    private int choiceAge;
+   private String efternavn;
+   private String fornavn;
    private String choiceActivePassive;
    private String choiceCompetitive;
    Scanner scan = new Scanner(System.in); //Opretter Scanner
 
    public void addMember(){
-   System.out.println("Du skal nu indtaste medlemmets oplysninger");
-   System.out.println("Indtast Fornavn:");
+	   System.out.println("Du skal nu indtaste medlemmets oplysninger");
+	   
+	   System.out.println("Indtast Fornavn: ");
+	   fornavn = scan.nextLine();
+	   System.out.println("Indtastet fornavn: " +fornavn);
    
-   System.out.println("Indtast Efternavn:");
+	   System.out.println("Indtast Efternavn:");
+	   efternavn = scan.nextLine();
+	   System.out.println("Indtastet fornavn : " +efternavn);
    
-   System.out.println("Skal medlemmet have aktivt eller passivt medlemsskab?");
-   choiceActivePassive = scan.nextLine();
-      if (choiceActivePassive.equalsIgnoreCase ("aktivt")){
-      
-      }
-      if (choiceActivePassive.equalsIgnoreCase ("passivt")){
-      
-      }
-      else{
-      System.out.println("Du har benyttet en ugyldig kommando, benyt venligst 'aktivt' eller 'passivt'");
-      }
+	   System.out.println("Skal medlemmet have aktivt eller passivt medlemsskab?");
+	   choiceActivePassive = scan.nextLine();
+	   if (choiceActivePassive.equalsIgnoreCase ("aktivt")){
+		   System.out.println("Aktivt medlemsskab registreret");
+	   }
+	   if (choiceActivePassive.equalsIgnoreCase ("passivt")){
+		   System.out.println("Passivt medlemsskab registreret");
+	   }
+	   else{
+		   System.out.println("Du har benyttet en ugyldig kommando, benyt venligst 'aktivt' eller 'passivt'");
+	   }
    
-   System.out.println("Tast 1 for under 18, 2 for over 18, og 3 for +60");
-   choiceAge = scan.nextInt();
-      if (choiceAge == (1)){
-         System.out.println("Du har valgt under 18");
-      }
-      if (choiceAge == (2)){
-         System.out.println("Du har valgt over 18");
-      }
-      if (choiceAge == (3)){
-         System.out.println("Du har valgt +60");
-      }
-      else{
-      System.out.println("Du har valgt en ugyldig kommando, benyt venligst følgende, og prøv igen: ");
-   }
+	   System.out.println("Tast 1 for under 18, 2 for over 18, og 3 for +60: ");
+	   choiceAge = scan.nextInt();
+	   if (choiceAge == (1)){
+		   System.out.println("Du har valgt under 18.");
+	   }
+	   if (choiceAge == (2)){
+		   System.out.println("Du har valgt over 18.");
+	   }	
+	   if (choiceAge == (3)){
+		   System.out.println("Du har valgt +60.");
+	   }
+	   else{
+		   System.out.println("Du har valgt en ugyldig kommando, benyt venligst fÃ¸lgende, og prÃ¸v igen: ");
+	   }
    
-   System.out.println("Er medlemmet motionist eller konkurrence svommer?");
-   choiceCompetitive = scan.nextLine();
-  if (choiceCompetitive.equalsIgnoreCase ("motionist")){
-      System.out.println("Du har valgt Motionist");
-   
-   }
-  if (choiceCompetitive.equalsIgnoreCase ("konkurrence")){
-      System.out.println("Du har valgt Konkurrence");
-   
-   }
-  else{
-   System.out.println("Du har skrevet en ugyldig kommando, benyt venligst 'motionist' eller 'konkurrence'");
-   System.out.println("Prøv venligst igen");
-   
-   }   
+	   System.out.println("Er medlemmet motionist eller konkurrence svommer?");
+	   choiceCompetitive = scan.nextLine();
+	   if (choiceCompetitive.equalsIgnoreCase ("motionist")){
+		   System.out.println("Du har valgt Motionist");
+	   }
+	   if (choiceCompetitive.equalsIgnoreCase ("konkurrence")){
+		   System.out.println("Du har valgt Konkurrence");
+	   }
+	   else{
+		   System.out.println("Du har skrevet en ugyldig kommando, benyt venligst 'motionist' eller 'konkurrence'");
+		   System.out.println("PrÃ¸v venligst igen");
+	   }   
   }
     
   public void deleteMember(){
   
   }
-
-
-
-
-
-
-
-
-
 }
