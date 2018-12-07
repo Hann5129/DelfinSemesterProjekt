6a167406bee19eb65c237d4d494b879d670c724c
sylv1634 <43637056+sylv1634@users.noreply.github.com>
2018-12-07 12:39:09 +0100

    Færdig + kommentarer

--- DelfinNyKode/Exerciser.java
+++ DelfinNyKode/Exerciser.java
@@ -2,6 +2,7 @@ public class Exerciser extends Member{
 
    // Sub Class constructor
    public Exerciser(String name,int age,String ageGroup, String activityStatus,String participationForm, int clubFee){
+      // Super Class constructor
       super(name, age, ageGroup, activityStatus, participationForm, clubFee);
    }
 }
