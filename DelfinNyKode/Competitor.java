6caf6b6bea44e535c39f20eb3d0039ef1fead0b0
sylv1634 <43637056+sylv1634@users.noreply.github.com>
2018-12-07 12:18:43 +0100

    Færdig + kommentarer

--- DelfinNyKode/Competitor.java
+++ DelfinNyKode/Competitor.java
@@ -2,6 +2,7 @@ public class Competitor extends Member{
    
    // Sub Class constructor
    public Competitor(String name,int age,String ageGroup, String activityStatus,String participationForm, int clubFee){
+      // Super class constructor
       super(name, age, ageGroup, activityStatus, participationForm, clubFee);
    }
 }
